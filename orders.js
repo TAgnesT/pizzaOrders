24314c18bd2395aa061ac6c486d1c1cd6e3bace5
TAgnesT <gmagiic@hotmail.com>
2022-05-10 19:42:38 +0200

    HTML and orders added

---
+++ orders.js
@@ -0,0 +1,51 @@
+const orders = [
+
+    {
+        id: 1,
+        pizza: "Hawaii",
+        exta: "ananas",
+        ital: "fanta",
+        price: 10,
+        kep: "https://www.pexels.com/photo/pizza-dish-367915/"
+    }
+    {
+        id: 2,
+        pizza: "Salami",
+        exta: "hot-salami",
+        ital: "coca-cola",
+        price: 9,
+        kep: "https://www.pexels.com/photo/photo-of-person-holding-sliced-pizza-3915857/"
+    }
+    {
+        id: 3,
+        pizza: "Ortolana",
+        exta: "ananas",
+        ital: "fanta",
+        price: 12,
+        kep: "https://www.pexels.com/photo/close-up-photo-of-pizza-2619970/"
+    }
+    {
+        id: 4,
+        pizza: "Broccoli",
+        exta: "panna",
+        ital: "sprite",
+        price: 10,
+        kep: "https://www.pexels.com/photo/baked-pizza-on-pizza-peel-in-oven-905847/"
+    }
+    {
+        id: 5,
+        pizza: "Green",
+        exta: "paprika",
+        ital: "coca-cola",
+        price: 8,
+        kep: "https://www.pexels.com/photo/a-colorful-sliced-pizza-2909822/"
+    }
+    {
+        id: 6,
+        pizza: "Muschroom",
+        exta: "funghi",
+        ital: "fanta",
+        price: 13,
+        kep: "https://www.pexels.com/photo/appetizing-pizza-with-shrimps-and-champignon-on-wooden-table-4553111/"
+    }
+]
