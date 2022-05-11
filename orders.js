efe9ae87f466afb235937c45a7a454216dd227a1
TAgnesT <105000830+TAgnesT@users.noreply.github.com>
2022-05-11 14:48:56 +0200

    All fixed JS CSS and HTML ready

--- orders.js
+++ orders.js
@@ -4,7 +4,7 @@ const orders = [
         id: 1,
         pizza: "Hawaii",
         exta: "ananas",
-        drink: "fanta",
+        drink: "Fanta",
         price: 10,
         kep: "https://www.simplehomecookedrecipes.com/wp-content/uploads/2021/02/Hawaiian-Pizza-800x530.png"
     },
@@ -12,7 +12,7 @@ const orders = [
         id: 2,
         pizza: "Salami",
         exta: "hot-salami",
-        drink: "coca-cola",
+        drink: "Coca-cola",
         price: 9,
         kep: "https://cdn.agrodolce.it/RWEFdFdF07lA6kJeVzMAu9ldF6w=/1150x647/smart/https://www.agrodolce.it/app/uploads/2014/03/shutterstock_130123424.jpg"
     },
@@ -20,7 +20,7 @@ const orders = [
         id: 3,
         pizza: "Ortolana",
         exta: "burrata",
-        drink: "fanta",
+        drink: "Coca-cola Zero",
         price: 12,
         kep: "https://shop.eismann.it/upload/it/17574-pizza-suprema-ortolana-con-burrata-K-20211022.jpg"
     },
@@ -28,7 +28,7 @@ const orders = [
         id: 4,
         pizza: "Broccoli",
         exta: "acciughe",
-        drink: "sprite",
+        drink: "Sprite",
         price: 10,
         kep: "https://media-assets.lacucinaitaliana.it/photos/620fc0f85d026d4777146e0c/1:1/w_1920,c_limit/Pizza-broccoli-e-acciughe.jpg"
     },
@@ -36,15 +36,15 @@ const orders = [
         id: 5,
         pizza: "Paprika",
         exta: "chili",
-        drink: "coca-cola",
+        drink: "Coca-cola",
         price: 8,
         kep: "https://blog.cookaround.com/vittoria70/wp-content/uploads/2016/05/DSCF5366.jpg"
     },
     {
         id: 6,
-        pizza: "Funghi",
+        pizza: "Mushroom",
         exta: "funghiporcini",
-        drink: "fanta",
+        drink: "Fanta",
         price: 15,
         kep: "https://www.cucina-naturale.it/wp-content/uploads/2017/09/funghi-zola.jpg"
     }
@@ -63,11 +63,11 @@ const orders = [
         
         output += `
         <div class="rendel">
-            <h3>Id:${orderek.id}</h3>
-            <h1>Pizza:${orderek.pizza}</h1>
-            <p>Extra:${orderek.exta}</p>
-            <p>Drink:${orderek.drink}</p>
-            <p>Price:${orderek.price}</p>
+            <h4>Id: ${orderek.id}</h4>
+            <h2>${orderek.pizza}</h2>
+            <p>Extra: ${orderek.exta}</p>
+            <p>Drink: ${orderek.drink}</p>
+            <p>Price: ${orderek.price} euro</p>
             <img src="${orderek.kep}" alt="" id="pics">
         </div>
     `;
